8d497f85a0ea0bfb331a2c9d1d66c84415f9645c
daniele vieira da silva <110404348+28122006dani@users.noreply.github.com>
2023-10-05 09:08:15 -0300

    0510

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
 alert("thor");
+
+document.querySelector('.tecla_pom').oncliik = tocaSomPom;
+function tocaSomPom(){document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_tim').oncliik = tocaSomtim;
+function tocaSomtim(){document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_puff').oncliik = tocaSompuff;
+function tocaSompuff(){document.querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla_splash').oncliik = tocaSomsplash;
+function tocaSomsplash(){document.querySelector('#som_tecla_splash').play();
+}
+
+document.querySelector('.tecla_toim').oncliik = tocaSomtoim;
+function tocaSomtoim(){document.querySelector('#som_tecla_toim').play();
+}
+
+document.querySelector('.tecla_psh').oncliik = tocaSompsh;
+function tocaSomtpsh(){document.querySelector('#som_tecla_psh').play();
+}
+
+document.querySelector('.tecla_ttic').oncliik = tocaSomttic;
+function tocaSomtic(){document.querySelector('#som_tecla_tic').play();
+}
+
+document.querySelector('.tecla_tom').oncliik = tocaSomtom;
+function tocaSomtom(){document.querySelector('#som_tecla_tom').play();
+}
+
+document.querySelector('.tecla_clap').oncliik = tocaSomclap;
+function tocaSomclap(){document.querySelector('#som_tecla_clap').play();
+}
+
